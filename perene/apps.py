6c55d6f48258fe563142698053f5803bcfa80bf7
hellosofi <scsb@cesar.school>
2026-09-18 18:39:13 -0300

    cria primeira aplicação do projeto

---
+++ perene/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PereneConfig(AppConfig):
+    name = 'perene'
--- projeto/settings.py
+++ projeto/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'perene',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
