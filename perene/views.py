d34b4e7f7797345129f730cd02345b34207862d7
hellosofi <scsb@cesar.school>
2026-09-19 14:32:01 -0300

    adiciona pastas template e static + estruturação da página inicial

--- perene/views.py
+++ perene/views.py
@@ -5,5 +5,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+# def index(request):
+#     return HttpResponse("aaaa")
+
 def index(request):
-    return HttpResponse("<h1>Aplicação Perene.</h1>")
+  	return render(request, 'index.html')
