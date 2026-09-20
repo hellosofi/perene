2e19ab8ca84f2462570b841aad6dbd91694e07bc
hellosofi <scsb@cesar.school>
2026-09-20 15:23:39 -0300

    cria modelos para Produtos

--- perene/models.py
+++ perene/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+import datetime
+from django.utils import timezone
+
+# class Produto(models.Model):
+#     nome = models.CharField(max_length=200, null=False)
+#     detalhe = models.TextField(null=False)
+#     validade = models.DateField('data', null=True, blank=True)
+
+#     def __str__(self):
+#         return self.nome
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=200, null=False)
+    detalhe = models.TextField(null=False)
+    validade = models.DateTimeField('data', null=True, blank=True)
+
+    def __str__(self):
+        return "[" + str(self.id) + "] " + self.nome
+    
+    # def status(self):
+    #     if self.data_criacao > timezone.now():
+    #         return "Vencido"
+    #     else:
+    #         return "Em análise"
+
+    def string_detalhada(self):
+        return "id: " + str(self.id) + "; nome: " + self.nome + "; detalhe: " + self.detalhe + "; validade: " + str(self.validade)
